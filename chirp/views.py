45480568d6e9621e9963ec64f1a5767cf741d93b
jbearer <jbearer@hmc.edu>
2016-04-15 20:08:29 -0700

    Fixed name conflicts

---
+++ chirp/views.py
@@ -0,0 +1,29 @@
+import os
+
+from firebase import firebase
+from flask import render_template, request, redirect, url_for, jsonify
+from gmusicapi import Mobileclient
+
+from chirp import app
+
+firebase = firebase.FirebaseApplication('https://musichack16.firebaseio.com/',
+                                        None)
+
+google = None
+
+@app.route('/')
+def login():
+    return render_template("login.html")
+
+@app.route('/search')
+def search():
+    RESULTS_PER_PAGE = 10
+
+    query = request.arg.get("query")
+
+    songResults = []
+    try:
+        songResults = google.search_all_access(query, max_results=RESULTS_PER_PAGE)
+    except:
+        songResults = google.get_all_songs(incremental=True)[:RESULTS_PER_PAGE]
+
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-from app import app
+from chirp import app
+import os
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
