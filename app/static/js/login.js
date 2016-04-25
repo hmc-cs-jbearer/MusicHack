3e12ab1e3b05c0917e158cdea4edab0b0607df9b
jbearer <jbearer@hmc.edu>
2016-04-24 19:37:37 -0700

    Fixed bug where user was taken to a blank page if they opened the site while they had a saved session

--- app/static/js/login.js
+++ app/static/js/login.js
@@ -1,5 +1,5 @@
 /**
- * A JSON object describing the current user. Contains the following fields:
+ * user: A JSON object describing the current user. Contains the following fields:
  * * uid: a unique user ID
  * * token: a Firebase token for the user
  * * expires: the time at which the user's session expires, in seconds since the
@@ -9,11 +9,17 @@
  * The object may contain additional fields specific to the provider. See
  * Firebase documentation.
  */
-
 var user = null;
 
+// Global Firebase reference that can be used throughout the project
 var firebase = new Firebase("https://musichack16.firebaseio.com/");
 
+/**
+ * Called on page load and ever time the user's authorization status
+ *  changes. Check that the user is logged in, and redirect to the
+ *  login page if necessary. If user is logged in, save user data
+ *  for later use by the page.
+ */
 firebase.onAuth(function(authData) {
     if (authData) {
 
