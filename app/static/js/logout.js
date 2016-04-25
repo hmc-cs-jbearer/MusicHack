07f670b86dd61d0e2405cdd0341a6944c0f4cda0
jbearer <jbearer@hmc.edu>
2016-04-24 22:48:51 -0700

    Logout from user page now works

---
+++ app/static/js/logout.js
@@ -0,0 +1,8 @@
+/**
+ * End the user's session. When the session ends, the onAuth callback associated
+ * with the Firebase reference will fire, causing the user to be redirected to
+ * the login page.
+ */
+function logout() {
+	firebase.unauth();
+}
